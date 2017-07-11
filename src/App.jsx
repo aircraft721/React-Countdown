5127c30e195671dcad44bb3f96c7355121ee0f92
Dan Constantinescu <dan_constantinescu@outlook.com>
2017-07-11 10:38:52 +0300

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import './app.css';
 
 
 class App extends Component {
     render(){
         return(
-            <div>Countdown Champ, App Component</div>
+            <div className='app'>
+                <div className='app-title'>Countown to October 23, 2017</div>
+                <div>
+                    <div className='clock-days'>14 days</div>
+                    <div className='clock-hours'>30 hours</div>
+                    <div className='clock-minutes'>15 minutes</div>
+                    <div className='clock-seconds'>20 seconds</div>
+                </div>
+                <div>
+                    <input type="text" placeholder='new date'/>
+                    <button>Submit</button>
+                </div>
+               
+            </div>
         )
         
     }
